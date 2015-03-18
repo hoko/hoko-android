816bad582587163ce365fa1c9a70941894ee8423
Ivan Bruel <ivan.bruel@gmail.com>
2015-03-18 14:33:14 +0000

    [Release] 1.1.3 Build

--- hoko/src/main/java/com/hokolinks/Hoko.java
+++ hoko/src/main/java/com/hokolinks/Hoko.java
@@ -29,7 +29,7 @@ import com.hokolinks.utils.versionchecker.HokoVersionChecker;
  */
 public class Hoko {
 
-    public static final String VERSION = "1.1.2";
+    public static final String VERSION = "1.1.3";
 
     // Static Instance
     private static Hoko mInstance;
