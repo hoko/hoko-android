fe0fc68705f2463844127c14d8589286f815ac02
Ivan Bruel <ivan.bruel@gmail.com>
2015-03-23 19:00:49 +0000

    [Added] Deferred Deeplinking

--- hoko/src/main/java/com/hokolinks/deeplinking/DeferredDeeplinkingBroadcastReceiver.java
+++ hoko/src/main/java/com/hokolinks/deeplinking/DeferredDeeplinkingBroadcastReceiver.java
@@ -3,8 +3,13 @@ package com.hokolinks.deeplinking;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 
+import com.hokolinks.Hoko;
 import com.hokolinks.utils.log.Log;
+
+import java.io.IOException;
+import java.net.URLDecoder;
 
 /**
  * Created by ivanbruel on 23/03/15.
@@ -14,6 +19,18 @@ public class DeferredDeeplinkingBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d(intent.toString());
+        String referrer = intent.getExtras().getString("referrer");
+        try {
+            referrer = URLDecoder.decode(referrer, "UTF-8");
+            Uri uri = Uri.parse("http://fakepath.com/query?" + referrer);
+            String deeplink = uri.getQueryParameter("utm_content");
+            Log.d("Opening deferred deeplink " + deeplink);
+
+            Hoko.deeplinking().openURL(deeplink);
+        } catch (IOException e) {
+            Log.e(e);
+        }
+
+
     }
 }
