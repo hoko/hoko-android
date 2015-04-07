222e7b162c2387fb2dc51c4ff4a575d6fe689933
Ivan Bruel <ivan.bruel@gmail.com>
2015-04-07 12:39:33 +0100

    Project changes

--- hoko/src/main/java/com/hokolinks/Hoko.java
+++ hoko/src/main/java/com/hokolinks/Hoko.java
@@ -30,7 +30,7 @@ import com.hokolinks.utils.versionchecker.VersionChecker;
  */
 public class Hoko {
 
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.2.1";
 
     // Static Instance
     private static Hoko mInstance;
