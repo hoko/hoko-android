1d2e35d68a852e82f3896b6494a1b7a1990a0699
Ivan Bruel <ivan.bruel@gmail.com>
2015-07-23 11:58:33 +0100

    Version number updated

--- hoko/src/main/java/com/hokolinks/Hoko.java
+++ hoko/src/main/java/com/hokolinks/Hoko.java
@@ -25,7 +25,7 @@ import com.hokolinks.utils.versionchecker.VersionChecker;
  */
 public class Hoko {
 
-    public static final String VERSION = "2.1.1";
+    public static final String VERSION = "2.1.2.1";
 
     // Static Instance
     private static Hoko sInstance;
