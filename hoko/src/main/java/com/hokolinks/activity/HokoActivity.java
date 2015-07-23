f340e5290ca36c083d4e7fdc6ad773f6716ab771
Ivan Bruel <ivan.bruel@gmail.com>
2015-07-23 11:22:11 +0100

    Release 2.1.2

--- hoko/src/main/java/com/hokolinks/activity/HokoActivity.java
+++ hoko/src/main/java/com/hokolinks/activity/HokoActivity.java
@@ -5,19 +5,37 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.hokolinks.Hoko;
+import com.hokolinks.deeplinking.listeners.SmartlinkResolveListener;
+
+import org.json.JSONObject;
 
 /**
  * HokoActivity serves the purpose of receiving incoming deeplinking intents and forwarding them to
  * the Deeplinking module where it will be parsed and start the associated activity.
  */
 @SuppressLint("Registered")
-public class HokoActivity extends Activity {
+public class HokoActivity extends Activity implements SmartlinkResolveListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String urlString = getIntent().getData().toString();
-
-        Hoko.deeplinking().openURL(urlString);
+        if (urlString.startsWith("http")) {
+            Hoko.deeplinking().openSmartlink(urlString, this);
+        } else {
+            Hoko.deeplinking().openURL(urlString);
+        }
     }
+
+    @Override
+    public void onLinkResolved(String deeplink, JSONObject metadata) {
+        finish();
+    }
+
+    @Override
+    public void onError(Exception e) {
+        finish();
+        Hoko.deeplinking().openURL(null);
+    }
+
 }
