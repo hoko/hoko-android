9278f37d3c158bc48ce1c8a3ccf21592a79e783b
ivanbruel <ivan.bruel@gmail.com>
2015-09-14 17:26:55 +0100

    Reverted automatic versioning

--- hoko/src/main/java/com/hokolinks/Hoko.java
+++ hoko/src/main/java/com/hokolinks/Hoko.java
@@ -25,7 +25,7 @@ import com.hokolinks.utils.versionchecker.VersionChecker;
  */
 public class Hoko {
 
-    public static final String VERSION = BuildConfig.VERSION_NAME.replace("-SNAPSHOT","");
+    public static final String VERSION = "2.3";
 
     // Static Instance
     private static Hoko sInstance;
